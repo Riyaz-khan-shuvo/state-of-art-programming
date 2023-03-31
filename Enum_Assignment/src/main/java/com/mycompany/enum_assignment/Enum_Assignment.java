c231f58078ea4b7df58134b216ad3a2a602d0928
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2023-04-01 05:40:30 +0600

    enum has done

---
+++ Enum_Assignment/src/main/java/com/mycompany/enum_assignment/Enum_Assignment.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.enum_assignment;
+
+/**
+ *
+ * @author Riyaz khan Shuvo
+ */
+public class Enum_Assignment {
+      enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+  }
+    
+
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM; 
+        System.out.println(myVar);
+        System.out.println("Hello World!");
+    }
+}
