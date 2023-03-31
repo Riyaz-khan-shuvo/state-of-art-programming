fffe206f3f916578ce2b11ae12d1094c4dc0a13d
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2023-04-01 05:42:35 +0600

    exception handaling has done

---
+++ Exception_Handaling/src/main/java/com/mycompany/exception_handaling/Exception_Handaling.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.exception_handaling;
+
+/**
+ *
+ * @author Riyaz khan Shuvo
+ */
+public class Exception_Handaling {
+
+    public static void main(String[] args) {
+         try {
+      int[] myNumbers = {1, 2, 3};
+      System.out.println(myNumbers[10]);
+    } catch (Exception e) {
+      System.out.println("Something went wrong.");
+    }
+    }
+}
